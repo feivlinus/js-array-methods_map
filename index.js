b238ace4d84992d5721100bfe92e7027e24c85cd
Felix Schmidt <para.feivl.dox@gmail.com>
2023-10-09 12:50:35 +0200

    Changed the solution acording to task

--- index.js
+++ index.js
@@ -24,35 +24,28 @@ const cards = [
   },
 ];
 
-const lowerCaseAnswers = []; // ['as often as you like.', ...]
-
-const questionsAndAnswersTogether = []; // ["How often can I use <header>? - As often as you like.", ...]
-
-const questionAndAnswer = []; // [{ question: 'How often can I use <header>?', answer: 'As often as you like.'}, {...}]
-
-export { lowerCaseAnswers, questionsAndAnswersTogether, questionAndAnswer };
-
 //Task 1
-
-cards.map((card) => {
-  lowerCaseAnswers.push(card.answer.toLowerCase());
+// ['as often as you like.', ...]
+const lowerCaseAnswers = cards.map((cards) => {
+  return cards.answer.toLowerCase();
 });
 
 console.log(lowerCaseAnswers);
 
 //Task 2
-cards.map((card) => {
-  questionsAndAnswersTogether.push(`${card.question} - ${card.answer}`);
+// ["How often can I use <header>? - As often as you like.", ...]
+const questionsAndAnswersTogether = cards.map((card) => {
+  return `${card.question} - ${card.answer}`;
 });
 
 console.log(questionsAndAnswersTogether);
 
 //Task 3
-cards.map((card) => {
-  questionAndAnswer.push({
-    question: card.question,
-    answer: card.answer,
-  });
+// [{ question: 'How often can I use <header>?', answer: 'As often as you like.'}, {...}]
+const questionAndAnswer = cards.map((card) => {
+  return { question: card.question, answer: card.answer };
 });
 
 console.log(questionAndAnswer);
+
+export { lowerCaseAnswers, questionsAndAnswersTogether, questionAndAnswer };
